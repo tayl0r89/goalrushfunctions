00b41aacd8738c16d3f8cdcd03b143667c6d624f
Steven Taylor <steven.taylor788@googlemail.com>
2020-11-13 20:54:10 +0000

    Adds data formatting util function

--- python/goalrush/data/csv_team_store.py
+++ python/goalrush/data/csv_team_store.py
@@ -6,6 +6,6 @@ class CsvTeamStore(TeamStore):
 
     def __init__(self, csv_file):
         temp = pd.read_csv(csv_file, header=0, names=["id", "name", "alias1", "alias2", "alias3"])
-        print(temp)
         temp['aliases'] = temp.apply(utils.combine_rows, axis=1)
         super().__init__(temp[['id','name','aliases']])
+
--- python/goalrush/data/utils.py
+++ python/goalrush/data/utils.py
@@ -5,9 +5,11 @@ def format_df(df, format):
         return df
     elif format == "list":
         return df_to_list(df)
+    else:
+        raise Exception("Format {fmt} not supported, should be either df or list,".format(fmt=format))
 
 def df_to_list(df):
-    pass
+    return list(df.to_dict(orient="index").values())
 
 def combine_rows(row):
     values = row.values
